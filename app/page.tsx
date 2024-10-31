9dcc367b4566ae7b9efb20ce13532948b4a236a3
Lucas Queiroz <lqsdesigner@gmail.com>
2024-10-31 09:42:57 -0300

    remove template config

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,23 @@ import Link from "next/link"
 export default function Home() {
   return (
     <div className="prose dark:prose-invert">
+      <div>
+        <h1 className="mt-6">Lucas Queiroz</h1>
+        <p>Crafting scalable and resilient software solutions using cutting-edge technologies and cloud-based architectures.</p>
+      </div>
+      <div>
+        <h2>Last posts</h2>
       {allPosts
       .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
       .map((post) => (
         <article key={post._id}>
           <Link href={post.slug}>
-            <h3>{post.title}</h3>
+            <h4 className="my-2">{post.title}</h4>
           </Link>
-          {post.description && <p>{post.description}</p>}
+          {post.description && <p className="m-0 p-0">{post.description}</p>}
         </article>
       ))}
+      </div>
     </div>
   )
 }
