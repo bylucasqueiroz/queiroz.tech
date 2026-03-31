8f1cd879f04a25e9a52b07662454983cfe8fb38a
Queiroz <lqsdesigner@gmail.com>
2026-03-30 22:24:15 -0300

    fix og size

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -6,10 +6,9 @@ export const runtime = 'edge';
 export async function GET(req: NextRequest) {
   const { searchParams } = req.nextUrl;
   const postTitle = searchParams.get('title');
-  const font = fetch(
-    new URL('../../public/fonts/kaisei-tokumin-bold.ttf', import.meta.url)
+  const fontData = await fetch(
+    'https://queiroz.tech/fonts/kaisei-tokumin-bold.ttf'
   ).then((res) => res.arrayBuffer());
-  const fontData = await font;
 
   return new ImageResponse(
     (
